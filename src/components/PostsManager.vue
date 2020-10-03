cf73e592bb27edb6fa419ccdd867b907824e689a
JoshuaMichaelHanson <josh@hansonslogic.com>
2020-10-03 17:06:54 -0500

    feat(HomePage) Homepage and navbar

---
+++ src/components/PostsManager.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container-fluid mt-4">
+    <h1 class="h1">Posts Manager</h1>
+    <p>Only authenticated users should see this page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostsManager'
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,48 +1,46 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Auth from '@okta/okta-vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Auth from "@okta/okta-vue";
 
 Vue.use(Auth, {
   issuer: process.env.VUE_APP_OKTA_DOMAIN,
   client_id: process.env.VUE_APP_CLIENT_ID,
-  redirectUri: window.location.origin + '/implicit/callback',
-  scope: 'openid profile email'
-})
+  redirectUri: window.location.origin + "/implicit/callback",
+  scope: "openid profile email",
+});
 
 //  redirectUri: window.location.origin + '/implicit/callback',
 // redirect_uri: 'http://localhost:3000/implicit/callback',
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/implicit/callback',
-    component: Auth.handleCallback()
+    path: "/implicit/callback",
+    component: Auth.handleCallback(),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: "/posts-manager",
+    name: "PostsManager",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../components/PostsManager.vue"),
     meta: {
-      requiresAuth: true
-    }
-  }
-]
+      requiresAuth: true,
+    },
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard());
 
-export default router
+export default router;
